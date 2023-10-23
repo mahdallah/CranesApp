a76b676ac8b8ec25ce669380544ee111ddb4e5fe
Abdirahman <mahdalle2001@gmail.com>
2023-10-23 11:41:50 +0300

    remove php UI

--- Cranes.UI.Php/Delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "MySql/Includes/dbh.inc.php";
-echo $id = $_GET['id'];
-echo $table = $_GET['table']."s";
-$q = "DELETE FROM `$table` WHERE `$table`.`Id` = $id";
-mysqli_query($con, $q);
-header("Location: http://" . $_SERVER['HTTP_HOST'] . "/php_projects/Hijaz/$table.php");
-exit;
-?>
